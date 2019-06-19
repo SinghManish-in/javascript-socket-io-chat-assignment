da9ad4240037f1064bc9009af1f795faa07f0aba
Singh <437038@cognizant.com>
2019-06-19 20:12:51 +0530

    all tested

--- public/js/script.js
+++ public/js/script.js
@@ -1,104 +1,103 @@
 function sendMessage(event, socket) {
 	event.preventDefault();
-	let channelName = document.getElementById('channel').value;
+	let channel = document.getElementById('channel').value;
 	let message = document.getElementById('message').value;
 	let username = document.getElementById('username').value;
 
-	//Display the Message
-	let div0 = document.createElement('div');
-	div0.className = 'col-12';
-
 	let div1 = document.createElement('div');
-	div1.className = 'card received-message';
+	div1.className = 'col-12';
 
 	let div2 = document.createElement('div');
-	div2.className = 'card-body';
+	div2.className = 'card received-message';
+
+	let div3 = document.createElement('div');
+	div3.className = 'card-body';
 
 	let p = document.createElement('p');
 	p.className = 'card-text';
 	p.innerHTML = `Me : ${message}`;
 
-	div2.appendChild(p);
+	div3.appendChild(p);
+	div2.appendChild(div3);
 	div1.appendChild(div2);
-	div0.appendChild(div1);
 
 	let root = document.getElementById('chatContainer');
-	root.insertBefore(div0, root.firstChild);
-	//Emit Message
-	socket.emit('message', {username:username, channel: channelName, message:message});
+	root.insertBefore(div1, root.firstChild);
+	
+	socket.emit('message', {username:username, channel: channel, message:message});
 }
 
 function joinChannel(event, socket) {
-	let channelsInpt = document.getElementById('newchannel').value;
-	socket.emit('joinChannel', {channel: channelsInpt});
+	let newchannel = document.getElementById('newchannel').value;
+	socket.emit('joinChannel', {channel: newchannel});
 
 }
 
 function leaveChannel(event, socket) {
-	let channelName = document.getElementById('newchannel').value;
-	socket.emit('leaveChannel', {channel: channelName});
+	let newChannel = document.getElementById('newchannel').value;
+	socket.emit('leaveChannel', {channel: newChannel});
 
 }
 
 function onWelcomeMessageReceived(message) {
-	let div0 = document.createElement('div');
-	div0.className = 'col-12';
-
 	let div1 = document.createElement('div');
-	div1.className = 'card received-message';
+	div1.className = 'col-12';
 
 	let div2 = document.createElement('div');
-	div2.className = 'card-body';
+	div2.className = 'card received-message';
+
+	let div3 = document.createElement('div');
+	div3.className = 'card-body';
 
 	let p = document.createElement('p');
 	p.className = 'card-text';
 	p.innerHTML = `System : ${message}`;
 
-	div2.appendChild(p);
+	div3.appendChild(p);
+	div2.appendChild(div3);
 	div1.appendChild(div2);
-	div0.appendChild(div1);
 
 	let root = document.getElementById('chatContainer');
-	root.appendChild(div0);
+	root.appendChild(div1);
 }
 
 function onNewMessageReceived(messageDetail) {
 	//Display the Message
-	let div0 = document.createElement('div');
-	div0.className = 'col-12';
-
 	let div1 = document.createElement('div');
-	div1.className = 'card received-message';
+	div1.className = 'col-12';
 
 	let div2 = document.createElement('div');
-	div2.className = 'card-body';
+	div2.className = 'card received-message';
+
+	let div3 = document.createElement('div');
+	div3.className = 'card-body';
 
 	let p = document.createElement('p');
 	let message = `${messageDetail.username} : ${messageDetail.message}`
 	p.className = 'card-text';
 	p.innerHTML = message;
 
-	div2.appendChild(p);
+	div3.appendChild(p);
+	div2.appendChild(div3);
 	div1.appendChild(div2);
-	div0.appendChild(div1);
 
 	let root = document.getElementById('chatContainer');
-	root.insertBefore(div0, root.firstChild);
+	root.insertBefore(div1, root.firstChild);
 }
 
 function onAddedToNewChannelReceived(channelDetail) {
-	let alertMsg = document.getElementById("alertContainer");
-	let dataList =  document.getElementById("channelsList");
-	let existingOptions = dataList.innerHTML;
+	let alertMessage = document.getElementById("alertContainer");
+	let channelsList =  document.getElementById("channelsList");
+	let existingOptions = channelsList.innerHTML;
 	let option='';
 	const channels = channelDetail.channel === '' ? [] : channelDetail.channel.split(',');
 	channels.forEach(channel =>{
 		option = option + `<option value="${channel}"/>`
 	})
 	//dataList.innerHTML = option;
-	dataList.innerHTML = option.concat(existingOptions);
+	channelsList.innerHTML = option.concat(existingOptions);
 	let msg = `You are added to <strong>${ channelDetail.channel }</strong> successfully!`
-	alertMsg.innerHTML = msg;
+	alertMessage.innerHTML = msg;
 }
 
 
