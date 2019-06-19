36948901c05bcf9fb14345c46932698f4aaea495
Singh <437038@cognizant.com>
2019-06-19 14:25:09 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 function bootstrapSocketServer(io) {
-	io.on('connection', () => {
+	console.log('bootstrapSocketServer')
+	io.on('connection', (socket) => {
+		socket.on('register', (regDetail) =>{
+		
+		socket.emit('addedToChannel',{channel: regDetail.channels});
+		socket.emit('welcomeMessage', `Welcome ${regDetail.username} !!`);
+		console.log('regDetail', regDetail,'regDetail.username',regDetail.username,'ch:',regDetail.channels)
+
+		socket.on('joinChannel', (channelName) =>{
+			socket.emit('addedToChannel',{channel: channelName.channel});
+		})
+
+		socket.on('leaveChannel', (channelName) =>{
+			socket.emit('removedFromChannel',{channel: channelName.channel});
+		})
+
+		socket.on('message', (messageDetail) =>{
+			socket.broadcast.emit('newMessage',messageDetail);
+		})
+
+		})
 	});
 }
 
