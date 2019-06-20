9ec51d93ab66476aabbaff51dcef474e2136c982
Singh <437038@cognizant.com>
2019-06-20 11:58:35 +0530

    logs removed

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 function bootstrapSocketServer(io) {
-	console.log('bootstrapSocketServer')
 	io.on('connection', (socket) => {
 		socket.on('register', (regDetail) =>{
 		
 		socket.emit('addedToChannel',{channel: regDetail.channels});
 		socket.emit('welcomeMessage', `Welcome ${regDetail.username} !!`);
-		console.log('regDetail', regDetail,'regDetail.username',regDetail.username,'ch:',regDetail.channels)
 
 		socket.on('joinChannel', (channelName) =>{
 			socket.emit('addedToChannel',{channel: channelName.channel});
